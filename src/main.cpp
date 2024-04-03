b65b1be338cb309c86fa4aa23f0da8287d6ca276
Evan <evanyou2004@gmail.com>
2024-04-02 22:01:29 -0500

    created tetris grid on screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
 #include <raylib.h>
+#include "grid.h"
 #define FPS 60
+
 int main() 
 {
+    Color darkGrey = {127, 127, 127, 255};
     InitWindow(300,600, "raylib Tetris");
     SetTargetFPS(FPS);
+    Grid grid = Grid();
+    grid.Print();
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
+        ClearBackground(darkGrey);
+        grid.Draw();
         EndDrawing();
     }
     CloseWindow();
