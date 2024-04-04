7cb553ae4be118dc96139fad7c631e0618adfc51
Evan <evanyou2004@gmail.com>
2024-04-03 21:02:36 -0500

    created block class and lblock.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,11 @@ int main()
     Color darkGrey = {127, 127, 127, 255};
     InitWindow(300,600, "raylib Tetris");
     SetTargetFPS(FPS);
+
     Grid grid = Grid();
+    grid.grid[0][0]= 1;
+    grid.grid[3][5]= 4;
+    grid.grid[17][8]= 7;
     grid.Print();
     while (WindowShouldClose() == false)
     {
